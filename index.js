0c403daeedb629ee79f1784dfeac4954964a47f2
Aaryan <aaryany.2505@gmail.com>
2025-08-16 17:39:37 +0530

    Created API

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const PORT =8000
+
+const books = [
+    {id:1,title:"book1",author:"Author1"},
+    {id:2,title:"book2",author:"Author2"},
+
+]
+///MiddleWares
+app.use(express.json())
+
+//Routes 
+app.get("/",(req,res)=>{
+    res.json(books)
+})
+app.get("/books/:id",(req,res)=>{
+    const id = req.params.id
+    const book = books.find((e)=>e.id==id)
+    if(isNaN(id)){
+        return res.status(400).json({error:"Bad Request !"})
+    }
+    if(!book)
+        return res.status(404).json({error:"Book not found !"})
+    return res.json(book)
+})
+app.post("/books",(req,res)=>{
+    const {title , author} = req.body
+    if(!title || title==='') 
+        return res.status(400).json({error:"title is required"})
+    if(!author || author==='') 
+        return res.status(400).json({error:"title is required"})
+const id = books.length+1
+const book ={id,title,author}
+    books.push(book)
+
+    return res.status(201).json({Meassge : "Book created Successfully "})
+})
+app.delete('/books/:id',(req,res)=>{
+    id = req.params.id
+    if(isNaN(id)){
+        return res.status(400).json({error:"Bad Request !"})
+    }
+    const IndextoDelete = books.find((e)=>e.id==id)
+    if(IndextoDelete<0)
+        return res.status(404).json({error:"Book does not exist "})
+
+books.splice(IndextoDelete,1)
+
+return res.json({delete : "deleted Successfully"})
+})
+app.listen(PORT,()=>{
+console.log(`Server is Running on ${PORT}`)
+})
